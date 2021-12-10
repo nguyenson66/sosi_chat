8ab29685f2f6ac73a3f4208576826ad32da801cf
Soi Con <nguyenson.66pt@gmail.com>
2021-12-10 12:58:06 +0700

    :v

--- src/routes/client.js
+++ src/routes/client.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router();
 
 
-router.get('', (req,res)=>{
-    res.send('ok')
+router.get('', (req, res) => {
+    res.send('Nguyen Hong Son')
 })
 
 module.exports = router
