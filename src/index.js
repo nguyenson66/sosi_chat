e7e65e425392852a7f4ec2dbf6541307da71a0ec
Soi Con <nguyenson.66pt@gmail.com>
2022-02-21 17:41:02 +0700

    test port 443

--- src/index.js
+++ src/index.js
@@ -1,39 +1,35 @@
-const express = require('express')
+const express = require('express');
 // const expressLayouts = require('express-ejs-layouts')
-const cookieParser = require('cookie-parser')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const path = require('path')
-const routes = require('./routes')
-const db = require('./config/connectDB')
-const dotenv = require('dotenv').config()
+const cookieParser = require('cookie-parser');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
+const routes = require('./routes');
+const db = require('./config/connectDB');
+const dotenv = require('dotenv').config();
 
+const socketIO = require('./config/socket');
 
-const socketIO = require('./config/socket')
-
-// setup 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname,'public')))
-app.use(cookieParser())
+// setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 db.connect();
 
 // set template engine
 // app.use(expressLayouts)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname + '/views'))
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/views'));
 
 // set routes
-routes(app)
-
+routes(app);
 
 ///////// socket ///
-socketIO(io)
+socketIO(io);
 /////////////
 
-
-server.listen(3000 , () => {
-    console.log('server is running')
-})
+server.listen(443, () => {
+    console.log('server is running');
+});
